2161e83030a6e50904e7d8d023daff221be4965b
anderml1955 <36250364+anderml1955@users.noreply.github.com>
2026-01-25 15:27:56 -0600

    Add files via upload

---
+++ setup-IP-CAN.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'Set MAC address'
+
+ip link set eth0 down
+ip link set eth0 address 2C:CF:67:06:95:F2
+ip link set eth0 up
+
+echo 'Set can0 to loopback'
+
+ip link set can0 down
+ip link set can0 up type can bitrate 1000000 loopback on
+ifconfig can0 txqueuelen 1000
+
+echo 'On PT dash restart daemon on the Network tab'
+
