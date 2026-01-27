1de650dcfd331840ad187f47591819d8c84418f2
anderml1955 <36250364+anderml1955@users.noreply.github.com>
2026-01-27 12:36:34 -0600

    Update setup-IP-CAN.sh

--- setup-IP-CAN.sh
+++ setup-IP-CAN.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
-echo 'Set MAC address'
+# run from /etc/init.d/powertune if you want it to run at startup
+#echo 'Set MAC address'
 
 ip link set eth0 down
 ip link set eth0 address 2C:CF:67:06:95:F2
 ip link set eth0 up
 
-echo 'Set can0 to loopback'
+#echo 'Set can0 to loopback'
 
 ip link set can0 down
 ip link set can0 up type can bitrate 1000000 loopback on
 ifconfig can0 txqueuelen 1000
 
-echo 'On PT dash restart daemon on the Network tab'
+#echo 'On PT dash restart daemon on the Network tab'
 
